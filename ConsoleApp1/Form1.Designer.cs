f279376a58368bb548ce540e4d6e840b1c595311
tasya132002@gmail.com <tasya132002@gmail.com>
2021-09-23 22:09:08 +0800

    Немного подкорректировализгачения на форме

--- ConsoleApp1/Form1.Designer.cs
+++ ConsoleApp1/Form1.Designer.cs
@@ -56,7 +56,7 @@ namespace ConsoleApp1
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 9F);
-            this.label2.Location = new System.Drawing.Point(211, 181);
+            this.label2.Location = new System.Drawing.Point(57, 184);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 14);
             this.label2.TabIndex = 2;
@@ -67,7 +67,7 @@ namespace ConsoleApp1
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calibri", 9F);
-            this.label3.Location = new System.Drawing.Point(211, 226);
+            this.label3.Location = new System.Drawing.Point(57, 232);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 14);
             this.label3.TabIndex = 3;
